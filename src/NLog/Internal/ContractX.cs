f9b7fc650b6cd752576ed1a5eaef4a075dba6602
Eric Newton <ericnewton76@gmail.com>
2018-12-27 13:48:08 -0500

    a class for contract enforcement

---
+++ src/NLog/Internal/ContractX.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    internal static class ContractX
+    {
+        /// <summary>
+        /// Asserts that the argument isnt null, throws ArgumentNullException if it is.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arg"></param>
+        /// <param name="paramName"></param>
+        /// <returns>the argument value</returns>
+        public static T ArgNotNull<T>(T arg, string paramName)
+        {
+            if(arg == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+
+            return arg;
+        }
+
+    }
+}
